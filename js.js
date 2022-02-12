78b4ad054321e3b117af0c1020136e30440f5031
Reginaldo Marinho <60780631+reginaldo-marinho@users.noreply.github.com>
2022-02-12 00:06:10 -0300

    commit

--- js.js
+++ js.js
@@ -1,7 +1,3 @@
 document.getElementById("btn-open-left").addEventListener("click",() => {
     document.getElementById("menu-left").style.width ="250px"
 });
-document.getElementById("btn-close-left").addEventListener("click",() => {
-    document.getElementById("menu-left").style.width ="0"
-});
-
