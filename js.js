339ad41d93cbbfd664704b3367e7cb8fc00c333d
Reginaldo Marinho <60780631+reginaldo-marinho@users.noreply.github.com>
2022-02-20 17:23:21 -0300

    commit

--- js.js
+++ js.js
@@ -1,3 +1,20 @@
-document.getElementById("btn-open-left").addEventListener("click",() => {
-    document.getElementById("menu-left").style.width ="250px"
+window.addEventListener("DOMContentLoaded", event => {
+    var section  = document.getElementsByTagName('section') ;
+    var sectionList = {}
+
+    Array.prototype.forEach.call(section,function (e){  
+        sectionList[e.id] = e.offsetTop  // pegas todas as posições atuais
+    });
+
+    document.addEventListener('scroll',function(){
+      var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop; 
+      for ( i in sectionList) {
+       if(sectionList[i] <= scrollPosition){ 
+           document.querySelector('.item-nav-ativo')
+           item = document.querySelector('a[href*=' + i + ']').setAttribute('class', 'item-nav-ativo');
+       }else{
+        document.querySelector('a[href*=' + i + ']').removeAttribute('class', 'item-nav-ativo');
+      }}
+    })
 });
+
